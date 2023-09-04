dc5df76b1584609fad3078a8c62c5d94b33f4eda
tamaritamari <shuhei.tamari@cureapp.jp>
2023-09-04 13:52:41 +0900

    https://the-guild.dev/graphql/codegen/docs/guides/react-vue

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+ 
+import './App.css'
+import Film from './Film'
+import { graphql } from '../src/gql'
+ 
+const allFilmsWithVariablesQueryDocument = graphql(/* GraphQL */ `
+  query allFilmsWithVariablesQuery($first: Int!) {
+    allFilms(first: $first) {
+      edges {
+        node {
+          ...FilmItem
+        }
+      }
+    }
+  }
+`)
+ 
+function App() {
+  // `data` is typed!
+  const { data } = useQuery(allFilmsWithVariablesQueryDocument, { variables: { first: 10 } })
+  return (
+    <div className="App">
+      {data && <ul>{data.allFilms?.edges?.map((e, i) => e?.node && <Film film={e?.node} key={`film-${i}`} />)}</ul>}
+    </div>
+  )
+}
+ 
+export default App
